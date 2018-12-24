e90edd3c1aaa03bbf26a7d751bb6b66b46e6bbaf
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2018-12-24 03:42:46 -0800

    pushing onto repo

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+
+
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: "./src/index.html",
+    filename: "./index.html"
+  });
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'components', 'App.js'),
+    // And to place the output in the `build` directory
+    output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'main.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+            test: /\.css$/,
+            use: [
+                {
+                    loader: "style-loader"
+                },
+                {
+                    loader: "css-loader",
+                    options: {
+                        modules: true,
+                        importLoaders: 1,
+                        localIdentName: "[name]_[local]_[hash:base64]",
+                        sourceMap: true,
+                        minimize: true
+                    }
+                },
+            ]
+        }
+      ]
+    },
+    plugins: [htmlPlugin]
+
+  };
