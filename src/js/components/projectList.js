3c232685d3c13261c22996b140be376e131fcac5
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2019-06-09 11:10:17 -0700

    am successfully iterating through a prop in my child component and have created projectList.js to be a modular component

--- src/js/components/projectList.js
+++ src/js/components/projectList.js
@@ -6,20 +6,20 @@ import {List, Segment } from 'semantic-ui-react'
 
 const ListExampleInverted = (props) => (
   <Segment inverted>
-    <div>
-    { console.log(props.projects) }
-    </div>
     <List divided inverted relaxed>
       <List.Item>
         <List.Header as="h2">{props.organization}</List.Header>
       </List.Item>
-      <List.Item>
-      <List.Content>
-          {props.projects}
-        </List.Content>
-      </List.Item>
+      {
+        props.projects.map((project, id)=>{
+          return <List.Item key={id}>
+            <List.Content>
+              <li>{project}</li>
+            </List.Content>
+          </List.Item>
+        })
+      }
     </List>
-    <Divider/>
   </Segment>
 )
   
