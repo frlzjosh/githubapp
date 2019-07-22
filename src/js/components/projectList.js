0e9ef9e52154ae857a14d1fe4d9dbb90b619cfff
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2019-07-22 00:32:22 -0700

    added descriptions to my project

--- src/js/components/projectList.js
+++ src/js/components/projectList.js
@@ -2,25 +2,48 @@ import React, {Component} from 'react'
 import ReactDOM from "react-dom"
 import Divider from './divider'
 
-import {List, Segment } from 'semantic-ui-react'
+import {List, Segment, Accordion, Icon} from 'semantic-ui-react'
 
 class ProjectList extends Component{
+  state = {
+    activeIndex :0
+  }
+  handleClick = (e, titleProps) => {
+    const { index } = titleProps
+    const { activeIndex } = this.state
+    const newIndex = activeIndex === index ? -1 : index
+
+    this.setState({ activeIndex: newIndex })
+  }
   render(){
     return (
-      <Segment inverted className="panel__card">
+      <Segment inverted className="project__card">
         <List divided inverted relaxed>
+        {
+          this.props.isHeaderNeeded === "true" ?
           <List.Item>
-            <List.Header as="h2">{this.props.organization}</List.Header>
+            <List.Header as="h1">{this.props.organization}</List.Header>
           </List.Item>
-          {
-            this.props.projects.map((project, id)=>{
-              return <List.Item key={id}>
-                <List.Content>
-                  <li>{project}</li>
-                </List.Content>
-              </List.Item>
-            })
-          }
+          : null
+        }
+        {
+          this.props.projects.map((project, id)=>{
+            return (
+            <List.Item key={id}>
+              <Accordion>
+                <Accordion.Title className="project__name" active={this.state.activeIndex === 0} index={0} onClick={this.handleClick}>
+                  <li className="project__name">{project} <p className="project__date">{this.props.date[id]}</p><Icon className="project__icon" name='dropdown' /></li> 
+                </Accordion.Title>
+                <Accordion.Content active={this.state.activeIndex === 0}>
+                  <p className="project__display-linebreak">
+                    {this.props.descriptions[id]}
+                  </p>
+                </Accordion.Content>
+              </Accordion>
+            </List.Item>
+            )
+          })
+        }
         </List>
       </Segment>
     )
--- src/js/views/App.js
+++ src/js/views/App.js
@@ -4,7 +4,7 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { Divider, Header} from 'semantic-ui-react'
+import { Divider, Header, Container} from 'semantic-ui-react'
 import ButtonEx from '../components/btn'
 import Panel from '../components/panel'
 import ProjectList from '../components/projectList'
@@ -15,40 +15,76 @@ class App extends React.Component {
         super(props);
         this.state = {
             metaLabProjects: ['iSTART', '10 Letters', 'META+LAB Website', 'META-UI Kit'],
+            metaLabProjectDescriptions: [
+                "- Retreived database queries using dependency injection through service/contract flow.\n- Created tests for functionality and data on Vue.js components as well as services and controllers with PHP unit tests.\n- Engineered the architecture and flow on how to efficiently implement and create our applications UI/UX.",
+                "- Refactored and optimized the front-end to store data with state management through Vue-x.\n- Managed front end functionality to be modular using Vue.js.\n- Implemented controller logic for database queries.",
+                "- Implemented an internal UI-Kit to create a modern-designed website",
+                "- Created themed and modular components for a UI-Kit"
+            ],
+            metaLabProjectDates: ['01/2019 - 05/2019', '12/2017 - 01/2019', '11/2017 - 02/2018', '08/2017 - 11/2017'],
             medtronicProjects: ['Patient Monitor'],
+            medtronicProjectDates: ['06/2019 - Present'],
+            medtronicProjectDescriptions: ["- Creating a system based off an event-driven architecture that reacts to a continuous stream of data in Go\n- Using Angular.js on the client-side to subscribe to incoming data from a web-socket\n- Node.js serves notifications to the users when events are triggered from the Go server"],
             personalProjects: ['My Portfolio', 'Scraps', 'Platelist'],
-            competitionsAndHackathons: ['LA Hacks', 'Bullring', 'Northrop Grumman Hackathon']
+            personalProjectDates: ['03/2019 - Present', '04/2019', '03/2019 - 05/2019'],
+            personalProjectDescriptions: [
+                "- Creating modular and reusable components in React.js",
+                "- Implemented several API's and libraries through Vue.js wrapper libraries.\n- Designed and developed a UI/UX into a functional product.",
+                "- Semi-finalist for CSUN's Bullring against other startup companies. \n- Developed an iOS/Android food platform called Platelist."
+            ],
+            competitionsAndHackathons: ['LA Hacks', 'Bullring', 'Northrop Grumman Hackathon'],
+            competitionsAndHackathonDates: ['04/2019', '02/2019 - 05/2019', '10/2017'],
+            competitionsAndHackathonDescriptions: [
+                "- Competed against teams in a 48-hour Hackathon to create products that will help or advance their city. Developed an app that uses machine learning, and geolocation to sustain pollution.",
+                "- Semi-finalist for CSUN's Bullring against other startup companies. Developed an iOS/Android food platform called Platelist.",
+                "- Won 2nd place in Northrop Grummans 24 hour Hackathon, versus students from universities in Southern California. We created a mobile web app using geolocation to help users."
+            ]
         }
     }
+
     render (){
         return (
             <div className="ui container">
                 <Navbar></Navbar>
-                <Header className="font--white" as="h2">Joshua J. Magdaleno's Portfolio</Header>
-                <ButtonEx/>
-                <Divider hidden />
-                <Panel
-                    header="Projects"
-                />
-                <ProjectList
-                    organization="Medtronic Inc."
-                    projects={this.state.medtronicProjects}
-                />
-                <ProjectList
-                    organization="META+LAB"
-                    projects={this.state.metaLabProjects}
-                />
-                <ProjectList
-                    organization="PERSONAL PROJECTS"
-                    projects={this.state.personalProjects}
-                />
-                <Panel
-                    header="Hackathon's & Competitions"
-                />
-                <ProjectList
-                    organization="PERSONAL PROJECTS"
-                    projects={this.state.competitionsAndHackathons}
-                />
+                <Container>
+                    <Header className="font--white" as="h2">Joshua J. Magdaleno's Portfolio</Header>
+                    <ButtonEx/>
+                    <Divider hidden />
+                    <Panel
+                        header="Projects"
+                    />
+                    <ProjectList
+                        organization="MEDTRONIC INC."
+                        projects={this.state.medtronicProjects}
+                        descriptions={this.state.medtronicProjectDescriptions}
+                        date = {this.state.medtronicProjectDates}
+                        isHeaderNeeded = "true"
+                    />
+                    <ProjectList
+                        organization="META+LAB"
+                        projects={this.state.metaLabProjects}
+                        descriptions={this.state.metaLabProjectDescriptions}
+                        date={this.state.metaLabProjectDates}
+                        isHeaderNeeded = "true"
+                    />
+                    <ProjectList
+                        organization="PERSONAL PROJECTS"
+                        projects={this.state.personalProjects}
+                        descriptions={this.state.personalProjectDescriptions}
+                        date={this.state.personalProjectDates}
+                        isHeaderNeeded = "true"
+                    />
+                    <Panel
+                        header="Hackathon's & Competitions"
+                    />
+                    <ProjectList
+                        organization="PERSONAL PROJECTS"
+                        projects={this.state.competitionsAndHackathons}
+                        descriptions={this.state.competitionsAndHackathonDescriptions}
+                        date={this.state.competitionsAndHackathonDates}
+                        isHeaderNeeded = "false"
+                    />
+                </Container>
             </div>
         );
     }
