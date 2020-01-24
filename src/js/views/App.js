a27ffaba5df003a6f4c8851fff1bfabc88f0166a
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2020-01-23 17:41:44 -0800

    updated how state is created for goals to get rid of bug

--- src/js/views/App.js
+++ src/js/views/App.js
@@ -2,7 +2,6 @@
 // importing components
 
 import React from 'react';
-import ReactDOM from "react-dom";
 import ButtonEx from '../components/btn'
 import {Route, Switch } from "react-router-dom";
 import Resume from './Resume'
@@ -16,7 +15,7 @@ export default function App() {
         <div>   
             <Container>
                 <Navbar></Navbar>
-                <Header className="font--white" as="h2">Joshua J. Magdaleno's Portfolio</Header>
+                <Header className="font--white" as="h1">Joshua J. Magdaleno's Portfolio</Header>
                     <ButtonEx/>
                     <Divider hidden />
             </Container>   
